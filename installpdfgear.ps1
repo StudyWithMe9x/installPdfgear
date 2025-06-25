03aacbc017ed5e16e637cc7dfa3ba60cc9b6cf89
StudyWithMe9x <remoteathena@gmail.com>
2025-06-25 14:44:03 +0700

    Update installpdfgear.ps1

--- installpdfgear.ps1
+++ installpdfgear.ps1
@@ -1,1 +1,3 @@
 
+<# 1. download and install pdfgear#>
+$Path = $env:TEMP; $Installer = "pdfgear.exe"; $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest "https://www.pdfgear.com/download/" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/y /silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer 
